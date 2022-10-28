9af7bb1b833ee23f96566e2e65a6b5e4c17956be
UBC Student <ymali@students.cs.ubc.ca>
2022-10-27 23:21:11 -0700

    Final 2

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -7,18 +7,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        User u1 = Interface.createUser();
-//
-//        Interface.createExercises(u1);
-//
-//        Interface.showExercises(u1);
-//
-//        Interface.removeExercise(u1);
-//
-//        Interface.showExercises(u1);
-//
-//        Interface.showProgress(u1);
-
         try {
             new Interface();
         } catch (FileNotFoundException e) {
