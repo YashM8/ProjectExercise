41cd1527ecff27188d3addab3c8dc2e77f17a9ff
UBC Student <ymali@students.cs.ubc.ca>
2022-10-28 00:41:18 -0700

    Final Push - 5

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -56,10 +56,24 @@ public class JsonReaderTest {
     }
 
     @Test
-    void testReadUser() {
+    void testReadUserEmpty() {
         JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
         try {
             User u1 = reader.read();
+            assertEquals("Bob", u1.getName());
+            assertEquals(170, u1.getHeight());
+            assertEquals(135, u1.getWeight());
+            assertTrue(u1.getExercises().isEmpty());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReadUserGeneral() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
+        try {
+            User u1 = reader.readUser();
             assertEquals("Bob", u1.getName());
             assertEquals(170, u1.getHeight());
             assertEquals(135, u1.getWeight());
